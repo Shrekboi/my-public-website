ef850b4fc7d76a2be6f12f728c65287dc00d78b6
Shrekboi <85759550+Shrekboi@users.noreply.github.com>
2022-01-17 13:47:16 +0200

    the site

---
+++ contact-page/send-email-script.js
@@ -0,0 +1,24 @@
+// import { PASSWD, EMAIL } from "./cridentials.js"
+
+
+// function sendEmail() {
+//     Email.send({
+//         Host: "smtp.gmail.com",
+//         Username: EMAIL,
+//         Password: PASSWD,
+//         To: EMAIL,
+//         From: document.getElementById("email").value,
+//         Subject: "New Contact Form Enquiry",
+//         Body: "Name: " + document.getElementById("name").value
+//             + "<br> Email: " + document.getElementById("email").value
+//             + "<br> Phone no: " + document.getElementById("phone").value
+//             + "<br> Message: " + document.getElementById("message").value
+//     }).then(
+//         message => {
+//             // const button = document.getElementById("button")
+//             // button.style = "background-color: red;"
+//             alert(message);
+//         }
+//     );
+// }
+
